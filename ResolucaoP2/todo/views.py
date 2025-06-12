cd89e1e58dbc77bdbc942fe1bb152f43f23193c7
ThiagoSanth <Thiagoalmeidasantanax@gmail.com>
2025-06-12 19:44:58 -0300

    Resolução 02 Criação e Configuração de template de Listagem

--- ResolucaoP2/todo/views.py
+++ ResolucaoP2/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Todo
+from django.views.generic import ListView, CreateView, UpdateView
 # Create your views here.
+
+
+class todoList(ListView):
+    model = Todo
+    template_name = 'todo/todo_list.html'
+    
